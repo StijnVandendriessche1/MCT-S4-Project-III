8554b62b2775f36728d1df4420c8097705577b9d
Tibovc <tibo.van.craenenbroeck@student.howest.be>
2020-05-19 09:23:59 +0200

    gitflow-feature-stash: InfluxDB

---
+++ Pi/Logic/mqtt.py
@@ -0,0 +1,37 @@
+""" Class for send to the mqtt-server
+pip install paho-mqtt """
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
+import json
+
+
+class MQTT:
+    def __init__(self, topic):
+        self.ip = "localhost"
+        self.port = 1883
+        self.topic = topic
+        self.qos = 1
+        self.start()
+
+    def start(self):
+        self.Client = mqtt.Client()
+        self.Client.on_connect = self.on_connect
+        self.Client.on_message = self.on_message
+
+        self.Client.connect(self.ip, self.port, 60)
+
+    def on_connect(self, client, userdata, flags, rc):
+        self.Client.subscribe(self.topic)
+
+    def on_message(self, client, userdata, msg):
+        pass
+
+    def publish(self, data):
+        objPayload = json.dumps(data)
+        self.Client.publish(self.topic, payload=objPayload,
+                            qos=self.qos, retain=False)
+
+""" test = MQTT("test")
+test_json = {"test": "Topi"}
+test.publish(test_json) """
