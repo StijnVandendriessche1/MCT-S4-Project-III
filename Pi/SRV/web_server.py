bfc1a8cbb4833871edf979c6f521fd935f57c4cc
Tibovc <tibo.van.craenenbroeck@student.howest.be>
2020-05-21 17:35:50 +0200

    gitflow-feature-stash: InfluxDB
    - JS
    - Influxdb wijzigingen

--- Pi/SRV/web_server.py
+++ Pi/SRV/web_server.py
@@ -2,15 +2,17 @@ from flask import Flask, jsonify, request
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
-import sys, os
+import sys
+import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_ROOT)
 sys.path.insert(0, BASE_DIR)
 
-for path in sys.path:
-    print(path)
-
 from Logic.server import Server
+import logging
+
+logging.basicConfig(filename="Logging.txt", level=logging.ERROR, format="%(asctime)s	%(levelname)s -- %(processName)s %(filename)s:%(lineno)s -- %(message)s")
+
 
 
 app = Flask(__name__)
@@ -20,40 +22,34 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 endpoint = '/api/v1'
 
-""" Vars """
-
-ai_meeting = True
-ai_coffee = True
-ai_dishwasher = True
+""" Objects """
+server = Server()
 
 
 """ Sockets """
 
+
 @socketio.on('connect')
 def connect():
-    socketio.emit('welcome', {'currentProgress': 0})
+    global server
+    socketio.emit('status_ai_meeting', {'status': server.status_ai["ai_meeting"]})
+
 
 """ Ai on or off """
 @socketio.on('ai_meeting')
-def connect():
-    global ai_meeting
-    if ai_meeting:
-        ai_meeting = False
-    else:
-        ai_meeting = True
-    socketio.emit('ai_meeting_status', {'status': ai_meeting})
+def ai_meeting():
+    global server
+    status = server.change_ai_status("ai_meeting")
+    socketio.emit('status_ai_meeting', {'status': status})
 
 
 """ Routes """
+
 
 @app.route('/')
 def hallo():
     return "Server is running"
 
-
-
-""" Class """
-server = Server()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
