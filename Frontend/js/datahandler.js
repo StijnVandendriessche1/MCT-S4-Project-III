0add8a0a344f73d223d340aa7906017488ee6bb6
Tibovc <tibo.van.craenenbroeck@student.howest.be>
2020-05-31 20:09:53 +0200

    gitflow-feature-stash: MeetingBoxes
    - Changed frontend 
    - Changed backend

--- Frontend/js/datahandler.js
+++ Frontend/js/datahandler.js
@@ -1,20 +1,16 @@
-const fetchaData = async function(lat, lon) {
-  return fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=lat&lon=lon&appid=66654d8c2851633f35104f156f49ca9a&units=metric&lang=nl&cnt=1`
-  )
-    .then(r => r.json())
-    .then(data => data);
+const fetchData = async function (url, method = "GET", body = null) {
+  return fetch(`${ip}/api/v1/${url}`, {
+    method: method,
+    body: body,
+    headers: { "content-type": "application/json" },
+  })
+    .then((r) => r.json())
+    .then((data) => data);
 };
-// 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
-let getAPI = async function(lat, lon) {
-  // Eerst bouwen we onze url op
-  // Met de fetch API proberen we de data op te halen.
-  // Als dat gelukt is, gaan we naar onze showResult functie.
+let getAPI = async function (url, callback, method = "GET", body = null) {
   try {
-    const data = await fetchaData(lat, lon);
-    showResult(data);
-    console.log(data);
+    const data = await fetchData(url, method, body);
+    callback(data);
   } catch (error) {
-    console.log(error);
   }
 };
