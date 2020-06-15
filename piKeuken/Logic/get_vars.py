b95008a0e6e8b118c767ec3acc818b13933126cf
Stijn Vandendriessche <stijn.vandendriessche3@student.howest.be>
2020-06-15 17:30:09 +0200

    Merge remote-tracking branch 'origin/develop' into develop
    fix server

--- piKeuken/Logic/get_vars.py
+++ piKeuken/Logic/get_vars.py
@@ -6,14 +6,14 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_ROOT)
 sys.path.insert(0, BASE_DIR)
 
-logging.basicConfig(filename="/home/pi/project3/data/logging.txt", level=logging.ERROR,
-                    format="%(asctime)s	%(levelname)s -- %(processName)s %(filename)s:%(lineno)s -- %(message)s")
+logging.basicConfig(filename=f"{BASE_DIR}/data/logging.txt", level=logging.ERROR,
+                    format="%(asctime)s    %(levelname)s -- %(processName)s %(filename)s:%(lineno)s -- %(message)s")
 
 class GetVars:
     def __init__(self):
         """ /home/pi/project3/settings.json 
             piKeuken\settings.json"""
-        self.file = "/home/pi/project3/settings.json"
+        self.file = f"{BASE_DIR}/settings.json"
         self.vars = self.start()
 
     def start(self):
